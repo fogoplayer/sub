a52b1c7ceaab337841af474a50adde628a5d85fd
fogoplayer <fogoplayer@gmail.com>
2017-09-01 13:53:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,66 @@
-var ref = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.0123456789";
-var save = new function(){
-  for(i = 0;i<ref.length();i++){
-    console.log(ref.substring(i,i+1));
-    var id = document.getElementById(ref.substring(i,i+1));
+var ref = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,!?0123456789";
+var storage = localStorage;
+
+//This function populates the input fields with whatever is saved in local 
+//storage 
+var load = function(){
+  //By default the storage object has 4 contents, I'm not sure why. If the
+  //storage object isn't empty, loop through storage and populate input fields
+  if(storage.length !== 4 ){
+    console.log("a");
+    for(i = 0;i<ref.length;i++){
+      var id = ref.substring(i,i+1);
+      var element = document.getElementById(id);
+      element.value=storage[id];
+    }
+  //Otherwise, populate storage with the default values and run the load 
+  //function again
+  }else{
+    for(i = 0;i<ref.length;i++){
+      var id = ref.substring(i,i+1);
+      localStorage.setItem(id, id);
+    }
+    load();
   }
-}
+};
+
+//Save the current contents of the input boxes to local storage
+var save = function(){
+  for(i = 0;i<ref.length;i++){
+    var id = ref.substring(i,i+1);
+    localStorage.setItem(id, document.getElementById(id).value);
+  }
+};
+
+//Encode wahtever is in the "input" div based on the rules in the input boxes
+var translate = function(){
+  //Save the current contents of the table
+  save();
+  //Bring in the contents of the input as a variable
+  var input = document.getElementById("input");
+  input = input.value;
+  input = input.toUpperCase();
+  //Bring in output as a variable
+  var output = document.getElementById("output");
+  output.value = "";
+  for(i=0;i<input.length;i++){
+    var id = input.substring(i, i+1);
+    if (id === " "){
+      output.value += localStorage.getItem("_");
+    }else if (localStorage.getItem(id) === null){
+      output.value += id;
+    }else{
+      output.value += localStorage.getItem(id);
+    }
+  }
+};
+
+//Run the load function on page load
+load();
+document.getElementById("translateButton").addEventListener("click", translate);
+document.getElementById("input").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode == 13) {
+        translate();
+    }
+});
