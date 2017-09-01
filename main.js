8974e624821ff4d08ba6312a5ce5a3995fd372f2
fogoplayer <fogoplayer@gmail.com>
2017-08-31 21:23:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var ref = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.0123456789";
 var save = new function(){
   for(i = 0;i<ref.length();i++){
-    console.log(ref.substring(i,i+1))
+    console.log(ref.substring(i,i+1));
     var id = document.getElementById(ref.substring(i,i+1));
   }
 }
